9141332b3f589a19255d8f83819df1d6227d9e34
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-10-06 16:31:49 -0300

    feat: complete the ListAllUsersUseCase class code

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,28 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  private usersRepository: IUsersRepository;
+
+  constructor(usersRepository: IUsersRepository) {
+    this.usersRepository = usersRepository;
+  }
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User not exists!");
+    }
+
+    const isAdmin = user.admin;
+
+    if (!isAdmin) {
+      throw new Error("The users list is only for admin users!");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
