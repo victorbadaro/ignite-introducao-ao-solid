85e42a454920590d806bbac6d7ed1182e07c3f29
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-10-06 16:41:22 -0300

    feat: complete the code for the TurnUserAdminController class

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -3,10 +3,22 @@ import { Request, Response } from "express";
 import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
 
 class TurnUserAdminController {
-  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
+  private turnUserAdminUseCase: TurnUserAdminUseCase;
+
+  constructor(turnUserAdminUseCase: TurnUserAdminUseCase) {
+    this.turnUserAdminUseCase = turnUserAdminUseCase;
+  }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    try {
+      const updatedUser = this.turnUserAdminUseCase.execute({ user_id });
+
+      return response.json(updatedUser);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
