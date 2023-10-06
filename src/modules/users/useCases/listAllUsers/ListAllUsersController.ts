bda3b09db36c3ca9000add24334c82f4422c3830
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-10-06 16:52:30 -0300

    feat: complete the ListAllUsersController class code

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,10 +3,22 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+  private listAllUsersUseCase: ListAllUsersUseCase;
+
+  constructor(listAllUsersUseCase: ListAllUsersUseCase) {
+    this.listAllUsersUseCase = listAllUsersUseCase;
+  }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id: `${user_id}` });
+
+      return response.json(users);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
